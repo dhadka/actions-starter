3905c79b4aeccecdd295b4b54f03f3bad3388fd0
Dave Hadka <dhadka@github.com>
2020-09-01 15:26:40 -0500

    Add README

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 
 import * as program from 'commander'
+import * as chalk from 'chalk'
 import * as fs from 'fs'
 import * as path from 'path'
 import * as execa from 'execa'
+import * as url from 'url'
 
 class StringOutput {
     value: string
@@ -202,10 +204,29 @@ jobs:
 `).toFile(".github/workflows/main.yml")
 }
 
-function writeReadme(name: string) {
+function writeReadme(name: string, repo?: string) {
     console.log("Writing README.md")
 
-    echo(`# ${name}`).toFile("README.md")
+    let content = `# ${name}`
+
+    if (repo) {
+        const repoUrl = url.parse(repo)
+        const path = repoUrl.path
+        const nwo = path?.substring(1, path.length-4)
+
+        content += `
+
+This is a starter workflow produced by [actions-starter](https://www.npmjs.com/package/actions-starter). To use this action, add the following step to your workflow:
+\`\`\`
+- name: Say hi to Dave
+  uses: ${nwo}@master
+  with:
+    name: Dave
+\`\`\`
+`
+    }
+
+    echo(content).toFile("README.md")
 }
 
 program
@@ -214,14 +235,25 @@ program
     .description("Generate starter projects for NodeJS, Typescript, or GitHub Actions")
 
 program
-    .command("open")
+    .command("edit")
+    .description("Edit project in VSCode")
     .action((inputs) => {
         execute("code", [process.cwd()])
     })
 
 program
+    .command("open")
+    .description("Open project on GitHub")
+    .action((inputs) => {
+        const repo = execute("git", ["remote", "get-url", "origin"])
+        const url = repo.endsWith(".git") ? repo.substring(0, repo.length-4) : repo
+        execute("start", ["/max", url])
+    })
+
+program
     .command("init")
-    .option('-t, --template <str>', 'The template to follow (default \'node\')')
+    .description("Initialize a new project")
+    .option('-t, --template <str>', 'The template to follow (default \'action\')')
     .option('-p, --project <str>', 'The name of the project')
     .option('-l, --license <str>', 'The license to use (default \'ISC\')')
     .option('-v, --initialVaersion <str>', 'The intial version (default \'1.0.0\')')
@@ -230,6 +262,7 @@ program
         let type = 'typescript'
         let name = path.basename(process.cwd())
         let repo: string | undefined = undefined
+        let branch: string | undefined = undefined
         let license = "ISC"
         let version = "1.0.0"
 
@@ -259,6 +292,8 @@ program
         if (inputs.repo) {
             repo = inputs.repo
         }
+
+        console.log(chalk.bold.underline(`Generating GitHub Actions project ${name}`))
         
         if (exists(".git")) {
             try {
@@ -273,6 +308,8 @@ program
                 execute("git", ["remote", "add", "origin", repo])
             }
         }
+
+        branch = execute("git", ["branch", "--show-current"])
         
         writePackageJson(name, repo, license, version)
         writeTslintJson()
@@ -281,7 +318,7 @@ program
         writePrettierRc()
         writeGitIgnore()
         writeActionYml(name)
-        writeReadme(name)
+        writeReadme(name, repo)
         mkdir("src")
         writeIndexJs()
         mkdir("src/__tests__")
@@ -290,6 +327,19 @@ program
         execute("npm", ["install", "@actions/core"])
         mkdir(".github/workflows")
         writeCIAction()
+
+        let counter = 0
+        console.log("")
+        console.log(chalk.bold.underline(`Your new Action is ready! To publish:`))
+        console.log(`${++counter}. Run your tests: ` + chalk.bold("npm test"))
+        console.log(`${++counter}. Build the action: ` + chalk.bold("npm run build"))
+        if (!repo) {
+            console.log(`${++counter}. Set your git remote: ` + chalk.bold("git remote add origin <url>"))
+        }
+        console.log(`${++counter}. Commit and push changes:`)
+        console.log("      " + chalk.bold("git add ."))
+        console.log("      " + chalk.bold(`git commit -m \"Publish version ${version}\"`))
+        console.log("      " + chalk.bold(`git push origin ${branch}`))
     })
 
 program.parse(process.argv);
